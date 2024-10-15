59e31c645f564b950a93df8307114f0e7c533d39
Ichate <146528530+Ichate@users.noreply.github.com>
2024-10-15 16:35:56 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# ShadowJourney API Client
+
+This Python library provides a client for interacting with the ShadowJourney API for chat completions and image generation.
+
+## Installation
+
+```bash
+pip install shadowjourney
---
+++ setup.py
@@ -0,0 +1,24 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='shadowjourney',
+    version='0.1.7',
+    description='shadowjourney API wrapper and cool stuff',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Ichate',
+    author_email='ichate_@outlook.com',
+    url='https://github.com/shadowjourneyy/shadowjourney',
+    packages=['shadowjourney'],
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
